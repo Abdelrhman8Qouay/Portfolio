e08cae105ad6e9c6e295d0d330a734f2bbc56e9e
abdelrhman ashraf <mido59478@gmail.com>
2022-12-25 16:36:09 +0200

    updated and added somethings with

--- master.js
+++ master.js
@@ -1,36 +1,25 @@
 
 let teckSkills = document.querySelectorAll(".containerTeckSkill .skills");
-let ldSkills = document.querySelector(".movingSkills");
+let ldSkills = document.querySelectorAll(".movingSkills");
 
-
+// Not Use Variables
 let goBtns = document.querySelector (".btns");
 let hireBtn = document.querySelector (".btns .hireBtn");
 let cvBtn = document.querySelector (".btns .cvBtn");
 
 window.onscroll = () => {
-    // for hire and cv buttons
-    if (window.scrollY >= goBtns.offsetTop ) {
-        hireBtn.classList.add("floating");
-        cvBtn.classList.add("floating");
-    }else {
-        hireBtn.classList.remove("floating");
-        cvBtn.classList.remove("floating");
-    }
-
-
     // for move at down
 
-    if (window.scrollY >= ldSkills.offsetTop - 500) {
-        teckSkills.forEach((skill) => {
+    // moving each skill alone with scrolling
+    teckSkills.forEach((skill) => {
+        if (skill.getBoundingClientRect().top < 700) {
             skill.style.width = skill.dataset.width;
             skill.textContent = skill.dataset.width;
-        })
-    } else if (window.scrollY < ldSkills.offsetTop - 500) {
-        teckSkills.forEach((skill) => {
+        } else if (skill.getBoundingClientRect().top > 700) {
             skill.style.width = "0";
             skill.textContent = "0%";
-        })
-    }
+        }
+    })
 }
 
 // for tooltip of bootstrap on page
